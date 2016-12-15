4535da41239dcaf99f36347b02d0cf7cb778099a
Sweeper777 <sumulang@gmail.com>
2016-12-15 07:23:55 +0000

    added testing code

--- DirectionalSnake/GameScene.swift
+++ DirectionalSnake/GameScene.swift
@@ -6,5 +6,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         bg = self.childNode(withName: "bg") as! SKSpriteNode
         
+        let system = GameSystem(boardSize: 750)
+        bg.addChild(system.boardNode)
     }
 }
