09914149a946a126a51c76b89af7786ddb40fa35
Sweeper777 <sumulang@gmail.com>
2016-12-17 08:20:13 +0000

    made gameSystem a class-level variable

--- DirectionalSnake/GameScene.swift
+++ DirectionalSnake/GameScene.swift
@@ -2,11 +2,13 @@ import SpriteKit
 
 class GameScene: SKScene {
     var bg: SKSpriteNode!
+    var gameSystem: GameSystem!
     
     override func didMove(to view: SKView) {
         bg = self.childNode(withName: "bg") as! SKSpriteNode
         
-        let system = GameSystem(boardSize: 750)
-        bg.addChild(system.boardNode)
+        gameSystem = GameSystem(boardSize: 750)
+        bg.addChild(gameSystem.boardNode)
+        
     }
 }
