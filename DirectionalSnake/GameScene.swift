cde832524c9c995e9837174224fdfb26d8ce9671
Sweeper777 <sumulang@gmail.com>
2016-12-17 08:21:17 +0000

    added gesture recognizers

--- DirectionalSnake/GameScene.swift
+++ DirectionalSnake/GameScene.swift
@@ -3,6 +3,10 @@ import SpriteKit
 class GameScene: SKScene {
     var bg: SKSpriteNode!
     var gameSystem: GameSystem!
+    var upRecog: UISwipeGestureRecognizer!
+    var downRecog: UISwipeGestureRecognizer!
+    var leftRecog: UISwipeGestureRecognizer!
+    var rightRecog: UISwipeGestureRecognizer!
     
     override func didMove(to view: SKView) {
         bg = self.childNode(withName: "bg") as! SKSpriteNode
@@ -10,5 +14,21 @@ class GameScene: SKScene {
         gameSystem = GameSystem(boardSize: 750)
         bg.addChild(gameSystem.boardNode)
         
+        upRecog = UISwipeGestureRecognizer(target: gameSystem, action: #selector(GameSystem.swipedUp))
+        upRecog.direction = .up
+        
+        downRecog = UISwipeGestureRecognizer(target: gameSystem, action: #selector(GameSystem.swipedDown))
+        downRecog.direction = .down
+        
+        leftRecog = UISwipeGestureRecognizer(target: gameSystem, action: #selector(GameSystem.swipedLeft))
+        leftRecog.direction = .left
+        
+        rightRecog = UISwipeGestureRecognizer(target: gameSystem, action: #selector(GameSystem.swipedRight))
+        rightRecog.direction = .right
+        
+        view.addGestureRecognizer(upRecog)
+        view.addGestureRecognizer(downRecog)
+        view.addGestureRecognizer(leftRecog)
+        view.addGestureRecognizer(rightRecog)
     }
 }
