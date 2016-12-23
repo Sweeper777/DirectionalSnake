1213c027cd56c36d7a9ece46964d5cf72a49c946
Sweeper777 <sumulang@gmail.com>
2016-12-23 08:55:19 +0800

    added basics of button node

---
+++ DirectionalSnake/ButtonNode.swift
@@ -0,0 +1,22 @@
+import SpriteKit
+
+class ButtonNode: SKSpriteNode {
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+        isUserInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        isUserInteractionEnabled = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
