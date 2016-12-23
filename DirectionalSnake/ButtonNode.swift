974ea386565d301340a3f328e2c57101567acb4f
Sweeper777 <sumulang@gmail.com>
2016-12-23 09:13:03 +0800

    changed the whole "change appearance" algorithm

--- DirectionalSnake/ButtonNode.swift
+++ DirectionalSnake/ButtonNode.swift
@@ -4,22 +4,44 @@ class ButtonNode: SKSpriteNode {
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
         super.init(texture: texture, color: color, size: size)
         isUserInteractionEnabled = true
+        originalColor = color
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         isUserInteractionEnabled = true
+        originalColor = color
     }
     
+    var originalColor: UIColor!
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        run(SKAction.colorize(with: UIColor.black, colorBlendFactor: 0.5, duration: 0))
+        if texture == nil {
+            let r = Float(originalColor.redComponent) / 255.0
+            let g = Float(originalColor.greenComponent) / 255.0
+            let b = Float(originalColor.blueComponent) / 255.0
+            let newR = r + 0.2 > 1 ? 1 : r + 0.2
+            let newG = g + 0.2 > 1 ? 1 : g + 0.2
+            let newB = b + 0.2 > 1 ? 1 : b + 0.2
+            color = UIColor(colorLiteralRed: newR, green: newG, blue: newB, alpha: 1)
+        } else {
+            run(SKAction.colorize(with: UIColor.black, colorBlendFactor: 0.5, duration: 0))
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        run(SKAction.colorize(withColorBlendFactor: 0, duration: 0))
+        if texture == nil {
+            color = originalColor
+        } else {
+            run(SKAction.colorize(withColorBlendFactor: 0, duration: 0))
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        run(SKAction.colorize(withColorBlendFactor: 0, duration: 0))
+        if texture == nil {
+            color = originalColor
+        } else {
+            run(SKAction.colorize(withColorBlendFactor: 0, duration: 0))
+        }
     }
 }
